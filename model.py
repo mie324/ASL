53d74b1963d0d4a183d2b1ee03158f58ce33a331
D-Hasan <danial.s.hasan@gmail.com>
2018-11-12 12:10:21 -0500

    Baseline model for testing training loop and establishing a starting case

---
+++ model.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+from torch import functional
+
+class ASLCNN(nn.Module):
+    def __init__(self):
+        self.output_size = 29
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 10, (5, 5), stride=2),
+            nn.ReLU(),
+            nn.Conv2d(10, 15, (10, 10), stride=2)
+            nn.MaxPool2d((2,2))
+            nn.Dropout(p=0.25)
+        )
+
+        self.linear = nn.Sequential(
+            nn.Linear(7260, 512),
+            nn.ReLU(),
+            nn.Linear(512, 64),
+            nn.ReLU(),
+            nn.Linear(64, self.output_size)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.linear(x)
+        return x
+
